1cf3cb2b6f6a7619f6081a54f97691a56a65e594
John Cockrum <johncockrum1@gmail.com>
2013-05-22 14:52:39 -0700

    working characters

--- app/models/character.rb
+++ app/models/character.rb
@@ -2,8 +2,7 @@ class Character < ActiveRecord::Base
   belongs_to :universe
   attr_accessible :description, :name, :archetype_attributes
     
-  has_many :archetypes
-  accepts_nested_attributes_for :archetypes, allow_destroy: true
-  
-  
+  has_one :archetype
+  accepts_nested_attributes_for :archetype, allow_destroy: true
+
 end
